664de389cf7ef9623f1887e0d3fbd9a465de91b2
Alex Leoniuk <alex.brest.by@gmail.com>
2022-09-07 15:58:15 +0300

    feat: add header

--- src/js/header.js
+++ src/js/header.js
@@ -7,19 +7,36 @@ header.appendChild(nav);
 
 const navLogo = document.createElement("div");
 navLogo.classList.add("nav-logo");
+
+const navHeader = document.createElement("div");
+navHeader.classList.add("nav-header");
+
 const navLogoImg = document.createElement("img");
 navLogoImg.classList.add("nav-logo__img");
 navLogoImg.src = `assets/images/miloserdie_logo.jpg`;
+
 navLogo.appendChild(navLogoImg);
 nav.appendChild(navLogo);
+
+const navHeaderH1 = document.createElement("h1");
+navHeaderH1.classList.add("nav-header__h1");
+navHeaderH1.innerHTML = `милосердие`;
+
+const navHeaderP = document.createElement("p");
+navHeaderP.classList.add("nav-header__p")
+navHeaderP.innerHTML = `православная служба помощи`;
+
+navHeader.appendChild(navHeaderH1);
+navHeader.append(navHeaderP);
+nav.appendChild(navHeader);
 
 const navUl = document.createElement("ul");
 navUl.classList.add("nav-list");
 nav.appendChild(navUl);
 
-const navArray = ["Главная", "Продукты", "Помочь", "Контакты"]
+const navArray = ["О службе", "Помощь бездомным", "Как помочь", "Новости", "Попросить о помощи"]
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 5; i++) {
     let navLi = document.createElement("li");
     navLi.classList.add("nav-item");
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 const main = document.createElement("div");
 main.classList.add("main");
-main.innerHTML = ``;
 
 export { main };
 
