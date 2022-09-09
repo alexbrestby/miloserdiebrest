8a0c5def9242bae09b4558bc68f2b8bc8ff06a31
Alex Leoniuk <alex.brest.by@gmail.com>
2022-09-09 21:16:18 +0300

    feat: card-slider improved

--- src/js/main-cards.js
+++ src/js/main-cards.js
@@ -4,13 +4,32 @@ mainCards.classList.add("main-cards");
 const mainCardsSlider = document.createElement("div");
 mainCardsSlider.classList.add("main-cards__slider", "cards-slider");
 
-const cardSliderNameArray = ["организация обедов", "банный день", "медобеспечение", "сбор одежды", "автобус милосердия", "восстановление документов"];
+const cardSliderItemTitlesArray = ["организация обедов", "банный день", "медобеспечение", "сбор одежды", "автобус милосердия", "восстановление документов"];
+
+const cardSliderItemLinksArray = ["organizaciya-obedov", "banniy-den", "medobespechenie", "sbor-odejdi", "avtobus-miloserdiya", "vosstanovlenie-doc"];
 
 
 for (let i = 0; i < 6; i++) {
   let cardSliderItem = document.createElement("div");
-  cardSliderItem.classList.add("cards-slider__item");
-  cardSliderItem.innerHTML = `${cardSliderNameArray[i]}`
+  cardSliderItem.classList.add("cards-slider__item", "card-item");
+
+  let cardSliderItemLink = document.createElement("a");
+  cardSliderItemLink.href = `${cardSliderItemLinksArray[i]}`;
+  cardSliderItemLink.target = "_blank";
+
+  let cardSliderItemImage = document.createElement("img");
+  cardSliderItemImage.classList.add("card-item__img");
+  cardSliderItemImage.src = `assets/images/slide_${[i + 5]}.jpg`;
+  cardSliderItemImage.alt = cardSliderItemLinksArray[i];
+  cardSliderItemLink.appendChild(cardSliderItemImage);
+
+
+  let cardSliderItemTitle = document.createElement("h2");
+  cardSliderItemTitle.classList.add("card-item__title");
+  cardSliderItemTitle.innerHTML = `${cardSliderItemTitlesArray[i]}`;
+  cardSliderItemLink.appendChild(cardSliderItemTitle);
+
+  cardSliderItem.appendChild(cardSliderItemLink);
   mainCardsSlider.appendChild(cardSliderItem);
 }
 
